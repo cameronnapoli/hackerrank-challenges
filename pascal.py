255f2cdd6d76d61565bc73d82eeb87b4acb17649
Cameron Napoli <cameronnapoli@gmail.com>
2017-06-19 13:23:50 -0700

    Pascals

---
+++ pascal.py
@@ -0,0 +1,14 @@
+# Generate a list of lists containing pascals triangle
+
+def pascal(p):
+    soln = []
+    for i in range(1,p+1):
+        if i == 1:
+            soln.append([1])
+        else:
+            next = []
+            prev = [0] + soln[-1] + [0]
+            for i in range(len(prev)-1):
+                next.append(prev[i] + prev[i+1])
+            soln.append(next)
+    return soln
